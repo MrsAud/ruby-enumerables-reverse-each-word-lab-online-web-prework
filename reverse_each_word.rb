e12606536b8be5b3ed1f69646c9bebf034762ae0
Audrea Cook <audreacwork@gmail.com>
2019-07-08 19:45:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
   
-  new_array.map |words |
+  new_array.map { |words| words.reverse}
   
   reversed = "#{new_array.join(" ")}"
   reversed
